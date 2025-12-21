f58dde2e349a79c638ee11ec68e62bc4310bb002
Shivani Surve <surveshivani99@gmail.com>
2025-12-21 14:38:18 +0530

    Multiplication

--- Assignment_01/04.Multiplication_Program.c
+++ Assignment_01/04.Multiplication_Program.c
@@ -3,16 +3,18 @@
 
 int main()
 {
-    int No1=0, No2=0, Ans=0;
+    int No1 = 0, No2 = 0, Ans = 0;
 
-    printf("\n Enter 1st Number=");
+    printf("\n Enter 1st Number = ");
     scanf("%d",&No1);
-    printf("\n Enter 2nd Number=");
+    
+    printf("\n Enter 2nd Number = ");
     scanf("%d",&No2);
 
-    Ans=No1*No2;
-    printf("\n Multiplication=>%d*%d=%d", No1, No2, Ans);
+    Ans = No1 * No2;
+    printf("\n Multiplication => %d * %d = %d", No1, No2, Ans);
 
     getch();
     return 0;
 }
+
