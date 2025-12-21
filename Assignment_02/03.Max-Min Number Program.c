9c1b4d3e84c38a264acc8ed63cf82e9b2258b0af
Shivani Surve <surveshivani99@gmail.com>
2025-12-21 14:44:24 +0530

    Max-Min Number Program

--- Assignment_02/03.Max-Min Number Program.c
+++ Assignment_02/03.Max-Min Number Program.c
@@ -4,21 +4,22 @@
 int main()
 {
     int No1, No2;
-    printf("\n Enter the Number: ");
+    printf("\n Enter the Number : ");
     scanf("%d",%No1,No2);
 
-    if(No1>No2)
+    if(No1 > No2)
     {
-        printf("\n First Number is Maximum than Second Number.");
-        printf("\n Second Number is Maximum than First Number.");
+        printf("\n First Number is Maximum than Second Number. ");
+        printf("\n Second Number is Maximum than First Number. ");
     }
-    else if(No2>No1)
+    else if(No2 > No1)
     {
-        printf("\n First Number is Minimum than Second Number.");
-        printf("\n Second Number is Minimum than First Number.");
+        printf("\n First Number is Minimum than Second Number. ");
+        printf("\n Second Number is Minimum than First Number. ");
     }
-    else if(No1=No2)
+    else if(No1 = No2)
     {
-        printf("\n Both Numbers are Equal.");
+        printf("\n Both Numbers are Equal. ");
     }
 }
+
