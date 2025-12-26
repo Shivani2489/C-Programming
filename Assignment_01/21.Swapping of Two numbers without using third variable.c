47001ffbfdde3f1ea4e9f6e3ed5f7f45ef8d9d0c
Shivani Surve <surveshivani99@gmail.com>
2025-12-26 21:54:55 +0530

    Add files via upload

---
+++ Assignment_01/21.Swapping of Two numbers without using third variable.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<conio.h>
+
+int  main()
+{
+    int No1 = 0, No2 = 0;
+
+    printf("\n Enter 1st Number : ");
+    scanf("%d",&No1);
+
+    printf("\n Enter 2nd Number : ");
+    scanf("%d",&No2);
+
+    printf("\n\n Numbers Before Swapping : No1 = %d, No2 = %d", No1, No2);
+
+    No1 = No1 + No2;
+	No2 = No1 - No2;
+	No1 = No1 - No2;
+
+    printf("\n\n Numbers After Swapping : No1 = %d, No2 = %d", No1, No2);
+
+    getch();
+    return 0;
+}
---
+++ Assignment_01/22.Swapping of Three Numbers.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    int No1 = 0, No2 = 0, No3 = 0, X = 0;
+
+    printf("\n Enter 1st Number : ");
+    scanf("%d",&No1);
+
+    printf("\n Enter 2nd Number : ");
+    scanf("%d",&No2);
+
+    printf("\n Enter 3rd Number : ");
+    scanf("%d",&No3);
+
+    printf("\n\n Numbers Before Swapping : No1 = %d, No2 = %d", No1, No2, No3);
+
+    X = No1;
+    No1 = No3;
+    No3 = No2;
+    No2 = X;
+
+    printf("\n\n Numbers After Swapping : No1 = %d, No2 = %d", No1, No2, No3);
+
+    getch();
+    return 0;
+}
