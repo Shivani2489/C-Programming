2e5a0d1578da5c92c46cee9d2ea84206d2dde2f3
Shivani Surve <surveshivani99@gmail.com>
2025-12-27 19:09:18 +0530

    Swapping of two numbers

--- Assignment_01/20.Swapping of Two Numbers accepts from user.c
+++ Assignment_01/20.Swapping of Two Numbers accepts from user.c
@@ -3,7 +3,7 @@
 
 int  main()
 {
-    int No1 = 0, No2 = 0, X = 0;
+    int No1 = 0, No2 = 0, Temp = 0;
 
     printf("\n Enter 1st Number : ");
     scanf("%d",&No1);
@@ -11,16 +11,15 @@ int  main()
     printf("\n Enter 2nd Number : ");
     scanf("%d",&No2);
 
-    printf("\n-----------------------------------------------------------");
-
     printf("\n\n Numbers Before Swapping : No1 = %d, No2 = %d", No1, No2);
 
-    X = No1;
+    Temp = No1;
     No1 = No2;
-    No2 = X;
+    No2 = Temp;
 
     printf("\n\n Numbers After Swapping : No1 = %d, No2 = %d", No1, No2);
 
     getch();
     return 0;
 }
+
