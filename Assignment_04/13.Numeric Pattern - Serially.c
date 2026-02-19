db16ce2524dabbe191729add96df43db575dbc7c
Shivani Surve <surveshivani99@gmail.com>
2026-02-19 12:10:45 +0530

    Add files via upload

---
+++ Assignment_04/13.Numeric Pattern - Serially.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    int i = 0, j = 0, RCnt = 0, CCnt = 0, No = 0;
+
+    printf("\n Enter the Row Count : ");
+    scanf("%d",&RCnt);
+    printf("\n Enter the Column Count : ");
+    scanf("%d",&CCnt);
+
+    printf("\n ====================================================\n\n");
+
+    for(No = 1, i = 1; i <= RCnt; i++)
+    {
+        for(j = 1; j <= CCnt; j++, No++)
+        {
+            printf(" %2d ",No);
+        }
+        printf("\n");
+    }
+
+    getch();
+    return 0;
+}
+
