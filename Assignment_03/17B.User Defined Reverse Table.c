1f23d895e82544be35e4cb488ad71991bbbdd631
Shivani Surve <surveshivani99@gmail.com>
2026-02-19 13:16:13 +0530

    Add files via upload

---
+++ Assignment_03/17B.User Defined Reverse Table.c
@@ -0,0 +1,28 @@
+///user defined
+
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+  int i = 0, j = 0, sPt = 0, ePt =0;
+
+  printf("\n Enter the Starting Tables's Number : ");
+  scanf("%d",&sPt);
+
+  printf("\n Enter the Ending Tables's Number : ");
+  scanf("%d",&ePt);
+
+  printf("\n ====================================================\n\n");
+
+  for(i = 1; i <= 10; i++)
+  {
+      for(j = sPt; j >= ePt; j--)
+      {
+        printf(" %3d ", i * j);
+      }
+      printf(" \n ");
+  }
+
+  getch();
+  return 0;
+}
---
+++ Assignment_03/18.Table 27 to 19.c
@@ -0,0 +1,24 @@
+/// 29 to 19
+
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+  int i = 0, j = 0;
+
+  printf("\n Tables from 27 to 19 is : \n");
+
+  printf("\n ====================================================\n\n");
+
+  for(i = 1; i <= 10; i++)
+  {
+      for(j = 27; j >= 19; j--)
+      {
+        printf(" %3d ", i*j);
+      }
+      printf("\n");
+  }
+
+  getch();
+  return 0;
+}
